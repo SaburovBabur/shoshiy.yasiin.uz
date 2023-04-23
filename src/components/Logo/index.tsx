481792468738eac2cccdc5f3a154fef4303c8f75
Saburov Babur <saburovbabur@gmail.com>
2023-04-23 15:28:31 +0500

    fix logo naming

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,7 +1,7 @@
 function Logo(props: any) {
 	return (
 		<h3 className="p !font-bold">
-			<span className="text-primary-def">/</span> Hokim shoshiy
+			<span className="text-primary-def">/</span> Hakim Shoshiy
 		</h3>
 	)
 }

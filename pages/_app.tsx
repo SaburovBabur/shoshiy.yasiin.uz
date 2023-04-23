8a4ca2bff9e80cfef82a4ba92c780a71d81bde5b
Saburov Babur <saburovbabur@gmail.com>
2023-04-23 11:52:43 +0500

    add analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { DM_Sans as Nunito } from 'next/font/google'
 import '@/styles/tailwind.css'
+import { Analytics } from '@vercel/analytics/react'
 
 const nunito = Nunito({ weight: ['400', '500', '700'], subsets: ['latin'] })
 
@@ -8,6 +9,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<div className={nunito.className}>
 			<Component {...pageProps} />
+
+			<Analytics />
 		</div>
 	)
 }

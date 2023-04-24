ad3dd3a904eefa70f690858647343756ef166e03
Saburov Babur <saburovbabur@gmail.com>
2023-04-24 21:22:42 +0500

    Update ya script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,9 +24,28 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 				<meta property="twitter:title" content="Hakim Shoshiy - Yaseen.uz ⚡" />
 				<meta property="twitter:description" content="Hakim Shoshiy - Yaseen.uz" />
 				<meta property="twitter:image" content="/bg.png" />
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+						
+						   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+						   m[i].l=1*new Date();
+						   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+						   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+						   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+						
+						   ym(93345331, "init", {
+								clickmap:true,
+								trackLinks:true,
+								accurateTrackBounce:true,
+								webvisor:true
+						   });
+						   
+							`,
+					}}
+				/>
 			</Head>
 			<div className={nunito.className}>
-				<YandexMetrika yid={93345331} clickmap={true} trackLinks={true} accurateTrackBounce={true} webvisor={true} />
 				<Component {...pageProps} />
 
 				<Analytics />

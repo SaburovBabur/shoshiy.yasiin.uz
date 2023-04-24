069b8fed4095efbf4e62a19ead3d24c48af58321
Saburov Babur <saburovbabur@gmail.com>
2023-04-24 14:08:18 +0500

    ADd metatags for shoshiy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,34 @@ import type { AppProps } from 'next/app'
 import { DM_Sans as Nunito } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 import '@/styles/tailwind.css'
+import Head from 'next/head'
 
 const nunito = Nunito({ weight: ['400', '500', '700'], subsets: ['latin'] })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<div className={nunito.className}>
-			<Component {...pageProps} />
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="theme-color" content="#000000" />
+				{/* <!-- Primary Meta Tags --> */}
+				<title>Hakim Shoshiy - Yaseen.uz ⚡</title>
+				<meta property="og:image" content="/bg.png" />
+				<meta name="title" content="Hakim Shoshiy - Yaseen.uz ⚡" />
+				<meta name="description" content="Hakim Shoshiy - Yaseen.uz" />
 
-			<Analytics />
-		</div>
+				{/* <!-- Twitter --> */}
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://www.Hakim Shoshiy - Yaseen.uz/" />
+				<meta property="twitter:title" content="Hakim Shoshiy - Yaseen.uz ⚡" />
+				<meta property="twitter:description" content="Hakim Shoshiy - Yaseen.uz" />
+				<meta property="twitter:image" content="/bg.png" />
+			</Head>
+			<div className={nunito.className}>
+				<Component {...pageProps} />
+
+				<Analytics />
+			</div>
+		</>
 	)
 }
